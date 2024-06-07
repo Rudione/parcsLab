4b8425166b69ff1ae13544acb20cc909da089280
Rudione <denisrudik01@gmail.com>
2024-06-07 13:08:08 +0300

    fix bugs

--- src/Bluck.java
+++ src/Bluck.java
@@ -1,14 +1,8 @@
 import parcs.*;
-import parcs.task;
-import parcs.AMInfo;
-import parcs.point;
-import parcs.channel;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 
 public class Bluck{
 
